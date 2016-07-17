2c6693c51c1d48f6f34a64eecb38f30a24c0567f
Adam Parsons <me@adamparsons.id.au>
2016-07-17 11:02:00 +0800

    Added uWSGI

--- anotherone/__init__.py
+++ anotherone/__init__.py
@@ -1,8 +1,8 @@
 # app/__init__.py
 from flask import Flask
 
-app = Flask(__name__)
-app.config.from_object('config')
+application = Flask(__name__)
+application.config.from_object('config')
 
 # Always import the views module after the flask app object has been created
 import anotherone.views
--- anotherone/views.py
+++ anotherone/views.py
@@ -1,18 +1,18 @@
-from anotherone import app
+from anotherone import application
 from flask import render_template, url_for
 import os
 from random import choice
 
-@app.route('/')
+@application.route('/')
 def index():
     selectedWisdom = randomImage()
     return render_template('index.html', selectedWisdom=selectedWisdom)
 
-@app.route('/name/<name>')
+@application.route('/name/<name>')
 def winmore(name):
     return render_template('hello.html', name=name)
 
 def randomImage():
-    nameList = os.listdir(os.path.join(app.static_folder, "img"))
+    nameList = os.listdir(os.path.join(application.static_folder, "img"))
     img_url = url_for('static', filename=os.path.join("img", choice(nameList)))
     return img_url
